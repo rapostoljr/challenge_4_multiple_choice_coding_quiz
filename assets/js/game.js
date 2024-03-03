02b34d21dd13e3731bb8e69ab928490f3719e5c9
RApostolJR <romuloapostol@gmail.com>
2024-03-02 19:33:51 -0800

    Added game.js, added classes to hopefully make the game.js run

---
+++ assets/js/game.js
@@ -0,0 +1,68 @@
+
+
+var questions = [ 
+{
+    question: 'Commonly used data types do not use.',
+    choice1: '1. strings',
+    choice2: '2. booleans',
+    choice3: '3. alerts',
+    choice4: '4. numbers',
+    answer: 3,
+},
+{
+    question: 'The condition in an if/else statement is enclosed with ______.',
+    choice1: '1. quotes',
+    choice2: '2. parenthesis',
+    choice3: '3. curly brackets',
+    choice4: '4. square brackets',
+    answer: 2,
+},
+{
+    question: 'Arrays in JavaScript can be used to store ______.',
+    choice1: '1. numbers and strings',
+    choice2: '2. other arrays',
+    choice3: '3. booleans',
+    choice4: '4. all of the above',
+    answer: 4,
+},
+{
+    question: 'String values must be enclosed within ______ when being assigned to variables.',
+    choice1: '1. quotes',
+    choice2: '2. curly brackets',
+    choice3: '3. parenthesis',
+    choice4: '4. commas',
+    answer: 1,
+},
+{
+    question: 'A very useful tool used during development and debugging for printing content to the debugger is:',
+    choice1: '1. JavaScript',
+    choice2: '2. terminal/bash',
+    choice3: '3. for loops',
+    choice4: '4. console.log',
+    answer: 4,
+},
+{
+    question: 'What repeats until a specified condition evaluates to false',
+    choice1: '1. my loops',
+    choice2: '2. for loops',
+    choice3: '3. while loops',
+    choice4: '4. froot loops',
+    answer: 2,
+},
+{
+    question: 'What does CSS stand for?',
+    choice1: '1. Caring, Sharing, Smiling',
+    choice2: "2. Can't Stand Still",
+    choice3: '3. Cascading Style Sheets',
+    choice4: '4. Coding, Styling, Scripting',
+    answer: 3,
+},
+{
+    question: 'What is the correct syntax for referring to an external script called "xxx.js"',
+    choice1: '1. <link rel="javascript" href="xxx.js" />',
+    choice2: '2. <script src="xxx.js" />',
+    choice3: '3. <script exec="xxx.js" />',
+    choice4: '4. <script rel="javascript" href="xxx.js" />',
+    answer: 1,
+}
+]
