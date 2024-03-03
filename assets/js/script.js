69f938d4a955e31640e2049026df2599e8ce035b
RApostolJR <romuloapostol@gmail.com>
2024-03-02 18:24:55 -0800

    added event listeners to script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,14 @@
 console.log("Hello world from the javascript")
 
 var startButton = document.querySelector(".button-start");
+var viewHighscoreButton = document.querySelector(".view-highscore-btn")
 
 startButton.addEventListener("click", function() {
-    console.log("Hello again");
+    console.log("Hello again from START button");
+    // need to create something to open the question-screen
+})
+
+viewHighscoreButton.addEventListener("click", function() {
+    console.log("Hello again from VIEW HIGHSCORE button");
+    // need to create something to open the highscores-screen
 })
