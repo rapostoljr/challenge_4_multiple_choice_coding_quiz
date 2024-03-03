8ffb6eadfbda5756a710f2c34df24031f3a445c8
RApostolJR <romuloapostol@gmail.com>
2024-03-02 18:17:33 -0800

    added a few lines of code in script.js, added more to the skeleton of the code in index.html, added a few more styles in style.css

---
+++ assets/js/script.js
@@ -0,0 +1,7 @@
+console.log("Hello world from the javascript")
+
+var startButton = document.querySelector(".button-start");
+
+startButton.addEventListener("click", function() {
+    console.log("Hello again");
+})
