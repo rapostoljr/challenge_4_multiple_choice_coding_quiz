b68f743760774323a4d9e67f86c8caf5566b8cc4
unknown <romuloapostol@gmail.com>
2024-03-02 23:50:55 -0800

    added event listeners to all buttons, buttons now go to correct screen, added ids to buttons

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,8 +1,13 @@
 console.log("Hello world from the javascript")
 
 var startButton = document.querySelector(".button-start");
-var viewHighscoreButton = document.querySelector(".view-highscore-btn")
+var viewHighscoreButton = document.querySelector(".view-highscore-btn");
 var timerCountdown = document.getElementById('timer-countdown');
+var beginQuizSection = document.getElementById('begin-quiz');
+var questionScreenSection = document.getElementById('question-screen');
+var highscoresScreenSection = document.getElementById('highscores-screen');
+var goBackBtn = document.getElementById('go-back-btn');
+var endQuizSection = document.getElementById('end-quiz');
 
 function countdown() {
     var timeLeft = 60;
@@ -20,9 +25,27 @@ startButton.addEventListener("click", function() {
     console.log("Hello again from START button");
     // need to create something to open the question-screen
     countdown();
+    beginQuizSection.style.display='none';
+    questionScreenSection.style.display='block';
+    endQuizSection.style.display='none';
+    highscoresScreenSection.style.display='none';
 })
 
 viewHighscoreButton.addEventListener("click", function() {
     console.log("Hello again from VIEW HIGHSCORE button");
     // need to create something to open the highscores-screen
+    beginQuizSection.style.display='none';
+    questionScreenSection.style.display='none';
+    endQuizSection.style.display='none';
+    highscoresScreenSection.style.display='block';
 })
+
+goBackBtn.addEventListener("click", function() {
+    console.log("Hello again from GO BACK button");
+    // need to create something to open the highscores-screen
+    beginQuizSection.style.display='';
+    questionScreenSection.style.display='none';
+    endQuizSection.style.display='none';
+    highscoresScreenSection.style.display='none';
+})
+
